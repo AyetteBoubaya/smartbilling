c88eeb4a5959311e1563576237c6dd646546bc09
AyetteBoubaya <boubayaayette@isima.u-monastir.tn>
2026-07-21 12:55:37 +0100

    feat(product): add Product antity and ProductCategory enum

---
+++ src/main/java/com/smartbilling/smartbilling/product/domain/Product.java
@@ -0,0 +1,39 @@
+package com.smartbilling.smartbilling.product.domain;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal priceHT;
+
+    @Column(nullable = false, precision = 5,scale = 2)
+    private BigDecimal taxRate;
+
+    @Column(length = 500)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
---
+++ src/main/java/com/smartbilling/smartbilling/product/domain/ProductCategory.java
@@ -0,0 +1,10 @@
+package com.smartbilling.smartbilling.product.domain;
+
+public enum ProductCategory {
+    SERVICE,
+    FOURNITURE,
+    LOGICIEL,
+    MATERIEL,
+    CONSEIL,
+    AUTRE
+}
